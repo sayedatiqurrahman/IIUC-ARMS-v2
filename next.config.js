bfed1bda16c27f218423cd770c840c5ddb5b0825
Sayed Atiqur Rahman <82232344+sayedatiqurrahman@users.noreply.github.com>
2026-07-23 17:14:47 +0600

    fix: add ui-avatars.com to next.config.js images

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
       { protocol: 'https', hostname: 'avatars.githubusercontent.com' },
       { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
       { protocol: 'https', hostname: '*.googleusercontent.com' },
+      { protocol: 'https', hostname: 'ui-avatars.com' },
     ],
   },
 };
