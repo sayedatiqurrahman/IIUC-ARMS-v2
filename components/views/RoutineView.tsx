2f4599adcde13c78acf40574cd93a672cb628163
Sayed Atiqur Rahman <82232344+sayedatiqurrahman@users.noreply.github.com>
2026-07-23 23:37:38 +0600

    fix: correct Arabic name of IIUC - الدوليّة (ad-Dawliyyah) to العالمية (al-Alamiyyah)

--- components/views/RoutineView.tsx
+++ components/views/RoutineView.tsx
@@ -428,7 +428,7 @@ const RoutinePrintView = forwardRef<HTMLDivElement, { routine: RoutineItem }>(({
             </div>
             <div className="routine-header-text">
               <h1 className="routine-university-name">{routine.university}</h1>
-              <p className="routine-arabic-name">&#x262F;&#x2015;&#x627;&#x644;&#x62C;&#x627;&#x645;&#x639;&#x629; &#x627;&#x644;&#x625;&#x633;&#x644;&#x627;&#x645;&#x64A;&#x629; &#x627;&#x644;&#x62F;&#x648;&#x644;&#x64A;&#x629; &#x634;&#x64A;&#x62A;&#x627;&#x63A;&#x648;&#x646;&#x63A;</p>
+              <p className="routine-arabic-name">&#x262F;&#x2015;&#x627;&#x644;&#x62C;&#x627;&#x645;&#x639;&#x629; &#x627;&#x644;&#x625;&#x633;&#x644;&#x627;&#x645;&#x64A;&#x629; &#x627;&#x644;&#x639;&#x644;&#x627;&#x645;&#x64A;&#x629; &#x634;&#x64A;&#x62A;&#x627;&#x63A;&#x648;&#x646;&#x63A;</p>
               <p className="routine-dept-name">{routine.department}</p>
             </div>
           </div>
