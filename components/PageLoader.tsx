b6808139e08bd108d3a4ce808a69931fca6ab5e4
sayedatiqurrahman <s.atiqurrahman2003@gmail.com>
2026-08-18 00:08:44 +0600

    style: remove white card from PageLoader, bump gif to w-40 h-40

--- components/PageLoader.tsx
+++ components/PageLoader.tsx
@@ -1,9 +1,7 @@
 export default function PageLoader({ className = '' }: { className?: string }) {
   return (
     <div className={`flex items-center justify-center ${className}`}>
-      <div className="bg-white rounded-3xl p-6 shadow-2xl">
-        <img src="/page-loader.gif" alt="Loading" className="w-20 h-20 object-contain" />
-      </div>
+      <img src="/page-loader.gif" alt="Loading" className="w-40 h-40 object-contain" />
     </div>
   );
 }
