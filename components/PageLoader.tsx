cc1aa575f52893680ead5c48c814b87d6c8fcea2
sayedatiqurrahman <s.atiqurrahman2003@gmail.com>
2026-08-18 00:10:28 +0600

    fix: restore white card in PageLoader

--- components/PageLoader.tsx
+++ components/PageLoader.tsx
@@ -1,7 +1,9 @@
 export default function PageLoader({ className = '' }: { className?: string }) {
   return (
     <div className={`flex items-center justify-center ${className}`}>
-      <img src="/page-loader.gif" alt="Loading" className="w-40 h-40 object-contain" />
+      <div className="bg-white rounded-3xl p-6 shadow-2xl">
+        <img src="/page-loader.gif" alt="Loading" className="w-40 h-40 object-contain" />
+      </div>
     </div>
   );
 }
