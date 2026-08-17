ffdfc0c0610de5270b587674acf93a28ebd7264f
sayedatiqurrahman <s.atiqurrahman2003@gmail.com>
2026-08-18 00:33:44 +0600

    style: bump PageLoader gif display size to w-60 h-60

--- components/PageLoader.tsx
+++ components/PageLoader.tsx
@@ -2,7 +2,7 @@ export default function PageLoader({ className = '' }: { className?: string }) {
   return (
     <div className={`flex items-center justify-center ${className}`}>
       <div className="bg-white rounded-3xl p-6 shadow-2xl">
-        <img src="/page-loader.gif" alt="Loading" className="w-40 h-40 object-contain" />
+        <img src="/page-loader.gif" alt="Loading" className="w-60 h-60 object-contain" />
       </div>
     </div>
   );
